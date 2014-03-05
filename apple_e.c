f43185c338193509f7ea9d9459f19176e713688a
Bill Yau <billyauhk@gmail.com>
2014-03-05 14:21:51 +0800

    Changed datatype.

--- apple_e.c
+++ apple_e.c
@@ -4,8 +4,8 @@
 
 int main(int argc, char* argv[]){
   int n,m,i,j;
-  long carry;
-  unsigned char* efrac;
+  unsigned long carry;
+  unsigned short* efrac;
 
   if(argc<2)fprintf(stderr,"Not enough argument!\n"),exit(-1);
 // Number of decimal digits we want to get
@@ -17,16 +17,16 @@ int main(int argc, char* argv[]){
   n *= log(10)/log(2);
   n++; // Always round up to get guard bit
   printf("%d binary bits\n",n);
-  n /= 8;
+  n /= sizeof(unsigned short)*8;
   n++; // Always round up to get guard byte
-  printf("%d integer(%lu)\n",n,sizeof(unsigned char));
+  printf("%d integer(%lu)\n",n,sizeof(unsigned short));
 // Prepare memory for the result
-  efrac = (unsigned char*) malloc(sizeof(unsigned char)*(n+1));
+  efrac = (unsigned short*) malloc(sizeof(unsigned short)*(n+1));
   for(i=m;i>=2;i--){
     // Division loop
     carry = 1;
     for(j=0;j<=n;j++){
-      carry <<= 8;
+      carry <<= sizeof(unsigned short)*8;
       carry += efrac[j];
       efrac[j] = carry/i;
       carry -= efrac[j]*i;
@@ -36,7 +36,7 @@ int main(int argc, char* argv[]){
 // Hex -> Decimal conversion could be done with dc
   printf("2.");
   for(i=0;i<n;i++){
-    printf("%02X",efrac[i]);
+    printf("%04X",efrac[i]);
   }
   putchar('\n');
   free(efrac);
