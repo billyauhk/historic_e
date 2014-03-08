c65bd11f6842444844ba7d3bac4632b63fb2c679
Bill Yau <billyauhk@gmail.com>
2014-03-08 15:21:45 +0800

    Decimal output for wozniak_e

--- wozniak_e.c
+++ wozniak_e.c
@@ -4,7 +4,7 @@
 #include<stdint.h>
 
 int main(int argc, char* argv[]){
-  int n,m,i,j;
+  int s,n,m,i,j;
   int test;
   uint64_t carry;
   uint32_t* efrac;
@@ -17,20 +17,19 @@ int main(int argc, char* argv[]){
   m = 5;
   test = (n+1)*2.30258509;
   while(m*(log(m)-1.0)+0.5*log(6.2831852*m) <= test){m++;}
-  m = n;
 // Number of binary bits we want to get
-  n *= log(10)/log(2);
-  n++; // Always round up to get guard bit
-  printf("%d binary bits\n",n);
-  n /= sizeof(uint32_t)*8;
-  n++; // Always round up to get guard byte
+  s = n*log(10)/log(2);
+  s++; // Always round up to get guard bit
+  printf("%d binary bits\n",s);
+  s /= sizeof(uint32_t)*8;
+  s++; // Always round up to get guard byte
   printf("%d integer(%lu)\n",n,sizeof(uint32_t));
 // Prepare memory for the result
-  efrac = (uint32_t*) malloc(sizeof(uint32_t)*(n+1));
+  efrac = (uint32_t*) malloc(sizeof(uint32_t)*(s+1));
   for(i=m;i>=2;i--){
     // Division loop
     carry = 1;
-    for(j=0;j<=n;j++){
+    for(j=0;j<=s;j++){
       carry <<= sizeof(uint32_t)*8;
       carry += efrac[j];
       efrac[j] = carry/i;
@@ -39,9 +38,24 @@ int main(int argc, char* argv[]){
   }
 // Output in hexadecimal
 // Hex -> Decimal conversion could be done with dc
+
+/*  printf("2.");
+  for(i=0;i<s;i++){
+    printf("%04X",efrac[i]);
+  }
+  putchar('\n');*/
+
+// Output in decimal
+// A rather slow approach, but not correct!?
   printf("2.");
   for(i=0;i<n;i++){
-    printf("%04X",efrac[i]);
+    carry = 0;
+    for(j=s;j>=0;j--){
+      carry += (uint64_t) efrac[j]*10;
+      efrac[j] = (uint32_t) carry&0xFFFFFFFF;
+      carry >>= 32;
+    }
+    putchar(carry+'0');
   }
   putchar('\n');
   free(efrac);
